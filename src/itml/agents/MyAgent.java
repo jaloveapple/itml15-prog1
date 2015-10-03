e413d5faae0c2b655759159a72a942511578d6ff
Arnar Freyr Bjarnason <arnarfreyrbjarnason@gmail.com>
2015-10-03 17:04:28 +0000

    Added random and aggressive move 'finder'

--- src/itml/agents/MyAgent.java
+++ src/itml/agents/MyAgent.java
@@ -9,6 +9,8 @@ import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
 import weka.core.Instance;
 import weka.core.Instances;
+import java.util.Random;
+
 
 import java.util.ArrayList;
 
@@ -83,10 +85,54 @@ public class MyAgent extends Agent{
         return null;
     }
 
-    private int calcDistanceBetweenAgents( StateBattle bs ) {
+    private Card randomMove( StateBattle stateBattle ){
+        StateAgent stateAgent = stateBattle.getAgentState( m_noThisAgent );
+        ArrayList<Card> cards = m_deck.getCards( stateAgent.getStaminaPoints() );
+        Random m_random = new Random();
+        return cards.get( m_random.nextInt( cards.size() ) );
+    }
 
-        StateAgent asFirst = bs.getAgentState( 0 );
-        StateAgent asSecond = bs.getAgentState( 1 );
+    private Card aggressiveMove( StateBattle stateBattle ){
+        StateAgent asThis = stateBattle.getAgentState( m_noThisAgent );
+        StateAgent asOpp  = stateBattle.getAgentState( m_noOpponentAgent );
+
+        ArrayList<Card> cards = m_deck.getCards( asThis.getStaminaPoints() );
+
+        // First check to see if we are in attack range, if so attack.
+        for ( Card card : cards ) {
+            if ( (card.getType() == Card.CardActionType.ctAttack) &&
+                    card.inAttackRange( asThis.getCol(), asThis.getRow(),
+                            asOpp.getCol(), asOpp.getRow() ) ) {
+                return card;  // attack!
+            }
+        }
+
+        // If we cannot attack, then try to move closer to the agent.
+        Card [] move = new Card[2];
+        move[m_noOpponentAgent] = new CardRest();
+
+        Card bestCard = new CardRest();
+        int  bestDistance = calcDistanceBetweenAgents( stateBattle );
+
+        // ... otherwise move closer to the opponent.
+        for ( Card card : cards ) {
+            StateBattle bs = (StateBattle) stateBattle.clone();   // close the state, as play( ) modifies it.
+            move[m_noThisAgent] = card;
+            bs.play( move );
+            int  distance = calcDistanceBetweenAgents( bs );
+            if ( distance < bestDistance ) {
+                bestCard = card;
+                bestDistance = distance;
+            }
+        }
+
+        return bestCard;
+    }
+
+    private int calcDistanceBetweenAgents( StateBattle stateBattle ) {
+
+        StateAgent asFirst = stateBattle.getAgentState( 0 );
+        StateAgent asSecond = stateBattle.getAgentState( 1 );
 
         return Math.abs( asFirst.getCol() - asSecond.getCol() ) + Math.abs( asFirst.getRow() - asSecond.getRow() );
     }
